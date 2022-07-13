c4b76e7af1b8f2ab98f252953bf6f73f50361046
Benjamin Bushman <benbushman98@gmail.com>
2022-07-12 18:33:36 -0600

    working on localstorage for events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
-const currentDayEl = $('#currentDay')
+let saveBtn = $('.saveBtn')
 
 
 timeHeader();
-
 function timeHeader() {
+    const currentDayEl = $('#currentDay')
     setInterval(() => {
     currentDayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-    }, 1000);
+    });
+}
+
+saveBtn = document.addEventListener("click", storeEvent)
+
+
+function storeEvent() {
+    var events = JSON.parse(window.localStorage.getItem("event")) || []
+    const eventEl = $('.description')
+
+    var eventText = {
+        eventname: eventEl.value
+    }
+    events.push(eventText)
+    window.localStorage.setItem("event", JSON.stringify(events));
 }
