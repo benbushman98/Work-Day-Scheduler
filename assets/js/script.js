07a9f952d3cacb2c4f94494cd27bc185223cda08
Benjamin Bushman <benbushman98@gmail.com>
2022-07-14 22:35:38 -0600

    updated comments in HTML and java

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,18 +21,22 @@ function init() {
 }
 
 $('.saveBtn').on("click", function() {
-    $('#9a').val(localStorage.setItem('9AM', $('#9a').val()))
-    $('#10a').val(localStorage.setItem('10AM', $('#10a').val()))
-    $('#11a').val(localStorage.setItem('11AM', $('#11a').val()))
-    $('#12p').val(localStorage.setItem('12PM', $('#12p').val()))
-    $('#1p').val(localStorage.setItem('1PM', $('#1p').val()))
-    $('#2p').val(localStorage.setItem('2PM', $('#2p').val()))
-    $('#3p').val(localStorage.setItem('3PM', $('#3p').val()))
-    $('#4p').val(localStorage.setItem('4PM', $('#4p').val()))
-    $('#5p').val(localStorage.setItem('5PM', $('#5p').val()))
+    $('#9a').val(localStorage.setItem('9AM', $('#9a').val()));
+    $('#10a').val(localStorage.setItem('10AM', $('#10a').val()));
+    $('#11a').val(localStorage.setItem('11AM', $('#11a').val()));
+    $('#12p').val(localStorage.setItem('12PM', $('#12p').val()));
+    $('#1p').val(localStorage.setItem('1PM', $('#1p').val()));
+    $('#2p').val(localStorage.setItem('2PM', $('#2p').val()));
+    $('#3p').val(localStorage.setItem('3PM', $('#3p').val()));
+    $('#4p').val(localStorage.setItem('4PM', $('#4p').val()));
+    $('#5p').val(localStorage.setItem('5PM', $('#5p').val()));
     init();
 })
 // End Local Storage Code
+
+
+
+// Code for Color Change
 timeChanger();
 function timeChanger () {
     today = moment();
@@ -56,3 +60,4 @@ function timeChanger () {
         console.log("future");
     }
 }
+// End of Code for Color Change
